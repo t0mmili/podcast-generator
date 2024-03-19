6f363e49bd22a142c9fb1db2af676ba36f131a12
Tomasz Fijarczyk <39084379+t0mmili@users.noreply.github.com>
2024-03-19 11:37:46 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,18 @@
 
 echo "============"
 
+echo "Configure git user name..."
 git config --global user.name "${GITHUB_ACTOR}"
+echo "Configure git user email..."
 git config --global user.email "${INPUT_EMAIL}"
+echo "Configure git safe directory..."
 git config --global --add safe.directory /github/workspace
 
+echo "Run feed update..."
 python3 /usr/bin/feed.py
 
+echo "Push changes to git..."
 git add -A && git commit -m "Update feed"
-
 git push --set-upstream origin main
 
 echo "============"
