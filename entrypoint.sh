5e136dce150abe31d119b34f1ba79cf011c47a70
Tomasz Fijarczyk <39084379+t0mmili@users.noreply.github.com>
2024-03-19 12:50:05 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,15 @@
 
 echo "============"
 
+# Workaround for broken HOME dir
 mkdir -p /root/snap/docker/2915/
 
-echo "Configure git user name..."
 git config --global user.name "${GITHUB_ACTOR}"
-echo "Configure git user email..."
 git config --global user.email "${INPUT_EMAIL}"
-echo "Configure git safe directory..."
 git config --global --add safe.directory /github/workspace
 
-echo "Run feed update..."
 python3 /usr/bin/feed.py
 
-echo "Push changes to git..."
 git add -A && git commit -m "Update feed"
 git push --set-upstream origin main
 
